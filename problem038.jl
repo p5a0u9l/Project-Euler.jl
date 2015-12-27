5abfbdf74e7489f3f7bb8571d2f42b285aa2a7b1
Paul Adams <pauladams509@gmail.com>
2015-12-26 21:16:23 -0800

    39 complete

--- problem038.jl
+++ problem038.jl
@@ -1,4 +1,19 @@
+#!/usr/local/bin/julia
 
+# Problem Statement
+println("Take the number 192 and multiply it by each of 1, 2, and 3:")
+println()
+println("192 × 1 = 192")
+println("192 × 2 = 384")
+println("192 × 3 = 576")
+println()
+println("By concatenating each product we get the 1 to 9 pandigital, 192384576. We will call 192384576 the concatenated product of 192 and (1,2,3)")
+println()
+println("The same can be achieved by starting with 9 and multiplying by 1, 2, 3, 4, and 5, giving the pandigital, 918273645, which is the concatenated product of 9 and (1,2,3,4,5).")
+println()
+println("What is the largest 1 to 9 pandigital 9-digit number that can be formed as the concatenated product of an integer with (1,2, ... , n) where n > 1?\n\n")
+
+# Solution
 function p038()
     for i = 9000:9999
         x = redigi(i, 2)
--- problem039.jl
+++ problem039.jl
@@ -1,40 +1,66 @@
+#!/usr/local/bin/julia
 
-function p039()
-    P = pythag_triplets(1000)
-    perims = P[:, 4]
-#     print(perims .== 120)
-    (r, count) = hist(perims, unique(perims))
-    println("$count")
-    
-    max_p = indmax(count)
-    println("p = $(max_p) maximizes number of solutions with $(count[max_p])")
-#     return count
-end
+# Problem Statement
+println("If p is the perimeter of a right angle triangle with integral length sides, {a,b,c}, there are exactly three solutions for p = 120.")
+println()
+println("{20,48,52}, {24,45,51}, {30,40,50}")
+println()
+println("For which value of p ≤ 1000, is the number of solutions maximised?\n\n")
 
-function pythag_triplets(N)
+# Solution
+# By Pythagorean's Theorem, the hypotenuse of a right triangle if given by 
+# c² = a² + b²  
+# If a, b, and c are integer they form a Pythagorean triplet.
+# Find all such triplets < N
+function find_pythag_triplets(N)
     count = 0
-    ptrips = zeros(Int, 200, 4)
-    for n = 1:30, m = 1:30
-        if m > n
-            (a, b, c, p) = euclid(m, n)
-            if p <= N
-                euc = sort([a, b, c, p])
-                if ~in(euc, ptrips)
+    K = round(Int, sqrt(N))    
+    all_pythag_triplets = zeros(Int, 400, 4)
+    for n = 1:K, m = n + 1:K
+        are_coprime = gcd(m, n) == 1
+        are_odd = (m - n) % 2 == 1
+        if are_coprime && are_odd 
+            k = 1
+            (a, b, c, p) = euclid(m, n, k)
+            while p <= N
+                euclid_entry = sort([a, b, c, p])
+                if ~in(euclid_entry, all_pythag_triplets)
                     count += 1
-                    ptrips[count, :] = euc
+                    all_pythag_triplets[count, :] = euclid_entry
                 end
+                k += 1
+                (a, b, c, p) = euclid(m, n, k)
             end
         end
     end
-    return ptrips[ptrips[:, 1] .!= 0, :]
+    return all_pythag_triplets[all_pythag_triplets[:, 1] .!= 0, :]
 end
 
-function euclid(m, n)
-    a = 2*m*n
-    b = m^2 - n^2
-    c = m^2 + n^2
+# Euclid's formula states that for an arbitrary pair of positive integers, m and n 
+# with m > n, m - n odd and m, n coprime, then the integers
+# a = k ⋅(m² - n²), b = k ⋅(2mn), c = k ⋅(m² + n²)
+# form all primitive Pythagorean triples and their multiples are generated with multiples of k
+# https://en.wikipedia.org/wiki/Pythagorean_triple
+function euclid(m, n, k)
+    b = k*(2*m*n)
+    a = k*(m^2 - n^2)
+    c = k*(m^2 + n^2)
     p = a+b+c
     a, b, c, p
 end
+  
+function main()
+    N = 1000
+    P = find_pythag_triplets(N)
+    P = sortrows(P, by = x -> x[4])
+    println("    a     b     c   Perimeter")
+    for p in range(1, size(P)[1]) 
+        @printf("%5.d %5.d %5.d %7.d\n", P[p, 1], P[p, 2], P[p, 3], P[p, 4])
+    end
+    perimeters = P[:, 4]
+    (r, count) = hist(perimeters, unique(perimeters))
+    i_max_p = indmax(count)
+    println("\np = $(r[i_max_p + 1]) maximizes the number of solutions with $(count[i_max_p])\n")
+end
 
-p = p039() 
+@time main()
