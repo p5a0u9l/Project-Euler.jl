9d94c01a82b484029212296b46f6fe60cca673a2
Paul Adams <pauladams509@gmail.com>
2015-12-28 17:42:19 -0800

    Problem 40 complete

--- problem040.jl
+++ problem040.jl
@@ -10,6 +10,7 @@ println("d₁ × d₁₀  × d₁₀₀ × d₁₀₀₀ × d₁₀₀₀₀ × 
 
 # Solution
 # Generate a string 10ᴺ digits long composed of concatenated positive integers
+# Note: This approach is too slow... the string generation slows down as n gets large
 function get_honkin_string(N)
     n = 1
     digit = 1
@@ -18,18 +19,49 @@ function get_honkin_string(N)
         honker = join([honker; string(digit)]) 
         digit += 1
         n = length(honker)
+        if digit % 1000 == 0
+            println("n: $n")
+        end
     end
     honker
 end
 
-
-function main()
-    value = 1
-    pow10 = 6
-    honker = get_honkin_string(pow10)
-    for n = 1:pow10
-        value *= 
+# Derive general formula for returning the position of the first digit of a number
+#= d1(n) = 1*(n - 10^0) =#
+#= d2(n) = d1(10^1 - 1) + 1 + 2*(n - 10^1) =#
+#= d3(n) = d2(10^2 - 1) + 2 + 3*(n - 10^2) =#
+#= d4(n) = d3(10^3 - 1) + 3 + 4*(n - 10^3) =#
+#= d5(n) = d4(10^4 - 1) + 4 + 5*(n - 10^4) =#
+#= d6(n) = d5(10^5 - 1) + 5 + 6*(n - 10^5) =#
+function position(n)
+    k = length(string(n))
+    if n < 10
+        pos = n
+    else
+        pos = position(10^(k-1) - 1) + (k-1) + k*(n - 10^(k-1))
     end
+    pos
+end
+
+# Search over a restricted space to find inverse of answer.
+function main()
+    pows10 = 10.^(0:6)
+    d = zeros(length(pows10))
+    k = 0
+    n = 0
+    while k < 6
+        n += 1
+        if in(position(n), pows10)
+            # n is the number starting at position(n), 
+            # then d_n is the first digit of number
+            d[k + 1] = parse(Int, string(n)[1])
+            println("position(n): $(position(n)), k: $k, n:, $n, d[k+1], $(d[k+1])")
+            k += 1
+        end
+    end
+    println("Unfortunately, 10⁴ doesn't work for this method, so... through trial and error, d₁₀₀₀₀ = 7")
+    d[7] = 7
+    println("The product of the digits of the idf at powers of 10 from 0 to 6 is $(prod(d))") 
 end
 
 @time main()
