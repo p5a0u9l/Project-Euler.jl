808ffce8542d895846a49f1a9a1d4a43dd7800b4
Paul Adams <pauladams509@gmail.com>
2017-03-18 20:58:29 -0700

    --wip-- [skip ci]

--- summarize.py
+++ summarize.py
@@ -6,11 +6,14 @@ import re
 BASEURL = "https://projecteuler.net/problem="
 
 with open("README.md", "w") as rme:
+    rme.write("| Problem    | Solution  | Latest Time |\n")
+    rme.write("| ---------- | --------  | ----------- |\n")
+
     for pr in glob("problem*jl"):
         problem_num = int(re.findall("\d+", pr)[0])
         url = "{}{}".format(BASEURL, problem_num)
         req = requests.get(url)
         soup = BeautifulSoup(req.text, "html5lib")
         problem_name = soup.findAll('h2')[0].text
-        rme.write("[{}]({}) --- [{}]({})\n\n".format(problem_name, url, pr, pr))
+        rme.write("| [{}]({}) | [{}]({}) |\n".format(problem_name, url, pr, pr))
 
