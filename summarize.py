9fd2f7d9692dc27fda5ff53e1f4a802797f26d45
Paul Adams <pauladams509@gmail.com>
2017-03-18 20:48:55 -0700

    add README

---
+++ summarize.py
@@ -0,0 +1,16 @@
+from bs4 import BeautifulSoup
+import requests
+from glob import glob
+import re
+
+BASEURL = "https://projecteuler.net/problem="
+
+with open("README.md", "w") as rme:
+    for pr in glob("problem*jl"):
+        problem_num = int(re.findall("\d+", pr)[0])
+        url = "{}{}".format(BASEURL, problem_num)
+        req = requests.get(url)
+        soup = BeautifulSoup(req.text, "html5lib")
+        problem_name = soup.findAll('h2')[0].text
+        rme.write("[{}]({}) --- [{}]({})\n".format(problem_name, url, pr, pr))
+
